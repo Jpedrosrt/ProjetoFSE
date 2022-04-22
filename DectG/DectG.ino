bc3d9423b3418b5ead831b7cc7c568591f45e267
João Pedro <j0310pedro@gmail.com>
2022-04-22 17:16:40 -0300

    implementação do buzzer

--- DectG/DectG.ino
+++ DectG/DectG.ino
@@ -1,32 +1,33 @@
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 9);
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int Buz = 10; 
-int Sgas = 2;
+int Sgas = 9;
 int Lgreen = 7;
 int Lred = 6;
 
 
 void setup() {
-  attachInterrupt(0, gasdetec, RISING);
-  attachInterrupt(0, gasndetec, FALLING);
-}
-
-void gasdetec() {
-  lcd.setCursor(0,0);
-  lcd.print("Gas Detected: Yes");
-  digitalWrite(Lred, HIGH);
-  digitalWrite(Lgreen, LOW);    
-}
-
-void gasndetec() {
   pinMode(Sgas, INPUT);
-  lcd.setCursor(0,0);
-  lcd.print("Gas Detected: No");
-  digitalWrite(Lgreen, HIGH);
-  digitalWrite(Lred, LOW);
+  pinMode(Buz, OUTPUT);
 }
 
 void loop() {
-
+  if(digitalRead(Sgas) == HIGH) {
+    lcd.setCursor(0,0);
+    lcd.print("  Gas Detected  ");
+    digitalWrite(Lred, HIGH);
+    digitalWrite(Buz, HIGH);
+    digitalWrite(Lgreen, LOW);
+  }
+  else {
+    lcd.setCursor(0,0);
+    lcd.print("Gas not Detected");
+    digitalWrite(Lred, LOW);
+    digitalWrite(Buz, LOW);
+    digitalWrite(Lgreen, HIGH);
+  }
+  delay(500);
+  lcd.clear();
 }
