8d3f4d19ac7bd6bcdfe56b5c8b06c13f896d51bf
João Pedro <j0310pedro@gmail.com>
2022-05-14 19:49:56 -0300

    vazamento por anomalia

--- Code/Constru__o_2/parte_const1/parte_const1.ino
+++ Code/Constru__o_2/parte_const1/parte_const1.ino
@@ -21,9 +21,15 @@ int typb = 13;
 int bvz;
 int b;
 int c;
+int d = 100;
+long timer;
+
+long timer2 = 0;
 bool gaswasdetc = false;
+bool aux = false;
 
 void setup() {
+  Serial.begin(9600);
   lcd.begin(16, 2);
   lcd.print("Detector de");
   lcd.setCursor(12,1);
@@ -45,13 +51,32 @@ void loop() {
   b = a/DIF;
   //Serial.println(bvz);
   c = ((b - bvz) * 100) / typb;
-  if(digitalRead(Sgas) == HIGH) {
+  
+  timer = millis();
+
+  if (timer == 80) {
+    timer2 = 0;
+  }
+  
+  if(c != d) {
+    d = c;
+    if((timer - timer2) < 6000) { //61200000
+      aux = true;
+    }
+    timer2 = timer;
+  }
+  
+  if(digitalRead(Sgas) == HIGH || aux) {
     digitalWrite(Lred, HIGH);
     digitalWrite(Lgreen, LOW);
     tone(Buz, 500);
     digitalWrite(valv, HIGH);
     lcd.setCursor(0,0);
-    lcd.print("  GAS DETECTED  ");
+    if(aux) { 
+      lcd.print("    GAS LEAK    ");
+    } else {
+      lcd.print("  GAS DETECTED  ");
+    }
     lcd.setCursor(0,1);
     gaswasdetc = true;
     lcd.print("LEVEL: ");
@@ -69,11 +94,10 @@ void loop() {
     digitalWrite(Lgreen, HIGH);
     digitalWrite(Lred, LOW);
     noTone(Buz);
-    if(!gaswasdetc) { 
-      lcd.setCursor(0,0);
+    lcd.setCursor(0,0);
+     if (!gaswasdetc){
       lcd.print("GAS NOT DETECTED");
     } else {
-      lcd.setCursor(0,0);
       lcd.print("GAS WAS DETECTED");
     }
     lcd.setCursor(0,1);
@@ -107,5 +131,6 @@ void calibrate() {
 
 void valvrest() {
   gaswasdetc = false;
+  aux = false;
   digitalWrite(valv, LOW);
 }
